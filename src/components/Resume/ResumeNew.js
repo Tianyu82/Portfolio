940f2cce93548ee6512a1c885de8b99f57ee99c2
unknown <feelingzty@gmail.com>
2024-09-13 13:48:02 -0700

    managed the pdf file

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,30 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
-import pdf from "../../Assets/../Assets/Tianyu_Resume.pdf";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+import pdf from "../../Assets/Tianyu_Resume.pdf"; // adjust path if needed
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <div>
       <Container fluid className="resume-section">
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+        <Row className="resume d-flex justify-content-center">
+          <embed
+            src={pdf}
+            type="application/pdf"
+            width="100%"
+            height="800px"
+          />
         </Row>
-
       </Container>
     </div>
   );
 }
 
 export default ResumeNew;
+
