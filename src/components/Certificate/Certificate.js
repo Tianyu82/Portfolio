fbca67132914c900930bc83c9a1a3d2f459aa906
unknown <feelingzty@gmail.com>
2024-11-27 23:36:38 -0800

    modified resume and added certificates and transcript

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import certificateImage from "../../Assets/certificate.jpg";
+import certificateImageISTQB from "../../Assets/ISTQB.jpg";
+import certificateImageSelenium from "../../Assets/CSETF.jpg";
+import schoolTranscript from "../../Assets/SFU.pdf";
 
 function Certificate() {
     return (
         <div>
             <Container fluid className="certificate-section">
-            <h2 className="certificate-heading">My Certifications</h2>
+            <h2 className="certificate-heading">Certifications and School Transcript</h2>
             <div style={{ marginLeft: "50px" }}>
                 <ul className="certificate-links">
                     <li>
-                    <a href={certificateImage} target="_blank" rel="noopener noreferrer"> ISTQB Certification (Click to View) </a>
+                    <a href={certificateImageISTQB} target="_blank" rel="noopener noreferrer"> ISTQB Certification (Click to View) </a>
                     </li>
                 </ul>
                 <div className="certificate-description">
@@ -18,6 +20,22 @@ function Certificate() {
                     <p> As of June 2023, ISTQB® has administered 1.3 million exams and issued more than 957k certifications in over 130 countries.</p>
                     <p> ISTQB® is one of the biggest and most established vendor-neutral professional certification schemes in the world. </p>
                 </div>
+                <ul className="certificate-links">
+                    <li>
+                    <a href={certificateImageSelenium} target="_blank" rel="noopener noreferrer"> Selenium Certification (Click to View) </a>
+                    </li>
+                </ul>
+                <div className="certificate-description">
+                    <p> The International Software Quality Institute (iSQI) is a leading provider of certification examinations all over the world. </p>
+                    <p> iSQI is one of the longest established and most trusted exam providers for worldwide-recognized certification.</p>
+                    <p> The A4Q Certified Selenium 4 Tester Foundation certification connects relevant test automation theory with practical application for the effective use of Selenium in creating automated tests for web applications. </p>
+                    <p> It is aimed at test professionals, test automation engineers, and developers who desire an understanding of how to design, implement, and maintain automated solutions using Selenium.</p>
+                </div>
+                <ul className="certificate-links">
+                    <li>
+                    <a href={schoolTranscript} target="_blank" rel="noopener noreferrer"> School Transcript (Click to View) </a>
+                    </li>
+                </ul>
             </div>
             
             </Container>
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -27,42 +27,42 @@ function ResumeNew() {
             <h3 style={{color:"black", fontWeight: "bold"}}>Technical Skills</h3>
             <ul>
               <li><strong>Languages:</strong> C/C++, Java, JavaScript, Python, SQL, HTML5, CSS3</li>
-              <li><strong>Frameworks and Libraries:</strong> React, Node.js, Spring Boot, Pandas, Numpy, Selenium Webdriver, Cypress</li>
+              <li><strong>Frameworks and Libraries:</strong> React, Node.js, Pandas, Selenium Webdriver, Cypress, Jest</li>
               <li><strong>Tools:</strong> Jira, Confluence, Git, Linux, Postman, Wireshark, AWS (Amplify, EC2, Lambda, DynamoDB)</li>
             </ul>
             <br />
 
             <h3 style={{color:"black", fontWeight: "bold"}}>Experience</h3>
               <h4 className="left-align">Test Automation Engineer (co-op)</h4>
-              <p>Vecima Networks, Burnaby, BC — Jan 2023 - Dec 2023</p>
+              <p>Vecima Networks, Burnaby, BC — Jan 2023 - Jan 2024</p>
               <ul>
-                <li>Conducted QA in a team of three, recognized for great performance with an extended internship invitation.</li>
+                <li>Conducted automation testing by clear and comprehensive test design, efficient test implementation, test execution, and test script maintenance. Met testing objectives all the time, and got an extended internship invitation.</li>
                 <li>Initiated an automation testing project for the company’s new platform using the JavaScript-based tool Sahi Pro. Set milestones for each phase to make sure the project always aligns with the goals.</li>
                 <li>Actively engaged with the development team to gather feedback, and iteratively refined test scripts based on their suggestions to make sure the project met the established benchmarks.</li>
                 <li>Transformed frequent device replacement tests from manual to automated, reducing 90% of testing time.</li>
-                <li>Effectively distributed test flows logically to multiple threads to improve the concurrency of test execution.</li>
-                <li>Applied Postman to API testing, uncovering hidden issues in APIs.</li>
+                <li>Effectively distributed test flows to multiple threads to reduce the repeated test execution and increase concurrency.</li>
+                <li>Used Postman on API testing and uncovered issues such as data inconsistency and status code errors in APIs.</li>
               </ul>
               <br></br>
 
               <h4 className="left-align">Software Test Engineer (co-op)</h4>
               <p>Gatekeeper Systems, Abbotsford, BC — Jan 2022 - Aug 2022</p>
               <ul>
-                <li>Conducted static testing on product specifications, proactively identifying and clarifying ambiguities</li>
-                <li>Analyzed product specifications to develop comprehensive test plans, including clear objectives, appropriate testing techniques, and detailed test cases</li>
-                <li>Collaborated with a 3-person team to conduct regression testing on firmware, web, and mobile applications, identifying over 50% of the bugs in the release, and ensuring comprehensive cross-platform verification</li>
-                <li>Logged detailed bug tickets in compliance with company documentation standards, and worked closely with developers and business stakeholders to resolve issues and meet product acceptance criteria</li>
-                <li>Conducted bug confirmation testing and user acceptance testing to ensure that bugs were fixed before release</li>
-                <li>Introduced Wireshark from school to the work projects, to analyze the packets between the server and the connected devices, resulting in the discovery and resolution of one long-lasting major bug</li>
+                <li>Conducted reviewing on product specifications at an early stage, proactively identifying and clarifying ambiguities.</li>
+                <li>Analyzed product specifications to develop comprehensive test plans, including clear objectives, appropriate testing techniques, and detailed test cases.</li>
+                <li>Collaborated with a 3-person team to conduct regression testing on firmware, web, and mobile applications, identifying over 50% of the bugs in the release, and ensuring comprehensive cross-platform verification.</li>
+                <li>Logged detailed bug tickets in compliance with company documentation standards, and worked closely with developers and business stakeholders to resolve issues and meet product acceptance criteria.</li>
+                <li>Conducted bug confirmation testing and user acceptance testing to ensure that bugs were fixed before release.</li>
+                <li>Introduced Wireshark tool from school projects to real work, to capture and analyze the packets on the server side, resulting in the discovery and resolution of one long-lasting major bug related to video downloading failure.</li>
               </ul>
               <br></br>
 
               <h4 className="left-align">CNC Programmer and CAD Drafter</h4>
               <p>Swiftsure Milling and Molding, Surrey, BC — Mar 2017 - Aug 2020</p>
               <ul>
-                <li>Drafted and finalized customer drawings using AutoCAD, and programmed a CNC router using Mastercam</li>
-                <li>Conducted quality control on multiple production lines to detect defects in wood products</li>
-                <li>Automated the calculation of sales and production metrics using VBA macros in Excel</li>
+                <li>Drafted and finalized customer drawings using AutoCAD, and programmed a CNC router using Mastercam.</li>
+                <li>Conducted quality control on multiple production lines to detect defects in wood products.</li>
+                <li>Automated the calculation of sales and production metrics using VBA macros in Excel, during the off-season.</li>
               </ul>
               <br></br>
 
@@ -71,10 +71,9 @@ function ResumeNew() {
               <h4 className="left-align">Full Stack App Deployment on AWS</h4>
               <p>Simon Fraser University, Burnaby, BC — Jan 2024 - Apr 2024</p>
               <ul>
-                <li>Collaborated with a team of 3 to design, develop, and deploy an application on AWS for note-taking and quiz generation.</li>
-                <li>Developed backend services using Node.js and contributed to frontend development with React.</li>
-                <li>Configured a load balancer on AWS Amplify to handle traffic and ensure high availability.</li>
-                <li>Deployed the application using AWS Lambda and microservices architecture to improve scalability.</li>
+                <li>Collaborated with a team of 3 to design, develop, and deploy a quiz application on AWS. Led the project thoroughly  using the Scrum framework by setting up weekly milestones and following up on the progress.</li>
+                <li>Developed all backend services using Nodejs and assisted in writing frontend using React to meet the deadline.</li>
+                <li>Deployed the application on AWS Lambda and split the application into loosely coupled microservices with replicas to increase availability and scalability.</li>
               </ul>
             </Col>
         </Row>
