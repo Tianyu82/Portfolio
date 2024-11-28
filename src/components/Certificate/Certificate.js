c6206909b12d17859f128b31fa7e3a25dba76755
unknown <feelingzty@gmail.com>
2024-11-28 01:06:09 -0800

    added pdf

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
+import { Document, Page, pdfjs  } from 'react-pdf';
 import certificateImageISTQB from "../../Assets/ISTQB.jpg";
 import certificateImageSelenium from "../../Assets/CSETF.jpg";
-import schoolTranscript from "../../Assets/SFU.pdf";
+// import schoolTranscript from "../../Assets/SFU.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 function Certificate() {
+    
+    const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	const onDocumentLoadSuccess = ({ numPages }) => {
+		setNumPages(numPages);
+	};
+
+	const goToPrevPage = () => setPageNumber((prevPage) => prevPage - 1);
+	const goToNextPage = () => setPageNumber((prevPage) => prevPage + 1);
+
     return (
         <div>
             <Container fluid className="certificate-section">
@@ -31,11 +45,28 @@ function Certificate() {
                     <p> The A4Q Certified Selenium 4 Tester Foundation certification connects relevant test automation theory with practical application for the effective use of Selenium in creating automated tests for web applications. </p>
                     <p> It is aimed at test professionals, test automation engineers, and developers who desire an understanding of how to design, implement, and maintain automated solutions using Selenium.</p>
                 </div>
-                <ul className="certificate-links">
-                    <li>
-                    <a href={schoolTranscript} target="_blank" rel="noopener noreferrer"> School Transcript (Click to View) </a>
-                    </li>
-                </ul>
+                <div>
+                    <nav>
+                        <button onClick={goToPrevPage}>Prev</button>
+                        <button onClick={goToNextPage}>Next</button>
+                    </nav>
+
+                    <div style={{ width: 600 }}>
+                    <Document
+                        file="/SFU.pdf"
+                        onLoadSuccess={onDocumentLoadSuccess}
+                        onLoadError={(error) =>
+                            console.error("Error loading PDF:", error)
+                        }
+                    >
+                            <Page pageNumber={pageNumber} width={600} />
+                        </Document>
+                    </div>
+
+                    <p>
+                        Page {pageNumber} of {numPages}
+                    </p>
+                </div>
             </div>
             
             </Container>
